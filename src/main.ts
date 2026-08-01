988545cdb2cde78ad8f020cd6f6a97c320c31425
IzharMohammed <izharmohammed21@gmail.com>
2026-08-01 22:57:06 +0530

    fix: add dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
+
+  const port = process.env.PORT ?? 4000;
+
+  await app.listen(port);
+
+  Logger.log(`Application running on http://localhost:${port}`);
 }
 
 void bootstrap();
